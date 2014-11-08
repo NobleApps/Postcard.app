f1f0a5450c4492ee45e23c1a6134b96d7c7bf0aa
Jack Noble <NobleAppsMac1@Jacks-MacBook-Pro.local>
2014-11-07 21:02:52 -0800

    updating the changes i did to github

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -38,9 +39,22 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
-        // Comment to test GitHub
+        
+        // Update app changes to GitHub
+
+    
+        
     }
 
 }
